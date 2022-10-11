5b50a6f52b4c749d06a77934bbe3b9bcce7747c8
krivtsov <krivtsov.su@gmail.com>
2022-10-12 00:21:32 +0400

    add type column

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,16 +1,29 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 import { hash } from 'bcrypt';
 
 @Entity('user')
+@Unique(['user_id'])
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   user_id: string;
+
   @Column()
   password: string;
+
   @BeforeInsert()
   async hashPAssword() {
     this.password = await hash(this.password, 10);
   }
+
+  @Column({ default: '' })
+  type: string;
 }
