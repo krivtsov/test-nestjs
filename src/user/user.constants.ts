063a1b1e7e741f2dbfdd6f2f4bc7298ad7c72bf2
krivtsov <krivtsov.su@gmail.com>
2022-10-12 00:18:13 +0400

    add user constants

---
+++ src/user/user.constants.ts
@@ -0,0 +1,4 @@
+export const ALREADY_REGISTRED_ERROR = 'User is found';
+export const USER_NOT_FOUND_ERROR = 'User not found';
+export const WRONG_PASSWORD_ERROR = 'Wrong password';
+export const NOT_VALID_ID_ERROR = 'Not Valid Id';
