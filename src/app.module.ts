e800eedab264cec899ef183fdd6f4f3e98be1f0d
krivtsov <krivtsov.su@gmail.com>
2022-10-12 10:19:48 +0400

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { getPgConfig } from './configs/pg.config';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
@@ -18,7 +16,7 @@ import { UserService } from './user/user.service';
     }),
     UserModule,
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
